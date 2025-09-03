9d424178b98d727497e17838eacb6db85ee60e93
jun1Nkz <juninkz@gmail.com>
2025-09-03 20:08:55 -0300

    Add files via upload

---
+++ SuperCityTrunfo.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+int main(){
+    char estado[20], código[10], cidade[20];
+    int população, pontos;
+    float área, PIB;
+
+    printf("Digite o nome do Estado: \n");
+    scanf("%s", &estado);
+
+    printf("Digite o codigo da carta: \n");
+    scanf("%s", &código);
+
+    printf("Digite o nome da Cidade: \n");
+    scanf("%s", &cidade);
+
+    printf("Digite a populacao: \n");
+    scanf("%d", &população);
+
+    printf("Digite a area em km2: \n");
+    scanf("%f", &área);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &PIB);
+
+    printf("Digite o numero de pontos turisticos: \n");
+    scanf("%d", &pontos);
+
+    printf("Cadastro Realizado com Sucesso!\n");
+    printf("  Carta 1\n");
+    printf("   Estado: %s\n", estado);
+    printf("   Codigo da Carta: %s\n", código);
+    printf("   Nome da Cidade: %s\n", cidade);
+    printf("   Populacao: %d\n", população);
+    printf("   Area em km2: %f\n", área);
+    printf("   PIB: %f\n", PIB);
+    printf("   Numero de Pontos Turisticos: %d\n", pontos);
+   
+
+        printf("Digite o nome do Estado: \n");
+    scanf("%s", &estado);
+
+    printf("Digite o codigo da carta: \n");
+    scanf("%s", &código);
+
+    printf("Digite o nome da Cidade: \n");
+    scanf("%s", &cidade);
+
+    printf("Digite a populacao: \n");
+    scanf("%d", &população);
+
+    printf("Digite a area em km2: \n");
+    scanf("%f", &área);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &PIB);
+
+    printf("Digite o numero de pontos turisticos: \n");
+    scanf("%d", &pontos);
+
+    printf("Cadastro Realizado com Sucesso!\n");
+    printf("  Carta 2\n");
+    printf("   Estado: %s\n", estado);
+    printf("   Código da Carta: %s\n", código);
+    printf("   Nome da Cidade: %s\n", cidade);
+    printf("   Populacao: %d\n", população);
+    printf("   Área em km2: %f\n", área);
+    printf("   PIB: %f\n", PIB);
+    printf("   Numero de Pontos Turísticos: %d\n", pontos);
+
+    
+    printf("Cadastro Finalizado, Obrigado!");
+}
+
+                                            
